d0274adaa64f7e7a49e76073302e58f546ea6a94
support-google-us <delfinatoniutti@sanluis.edu.ar>
2025-05-15 05:34:47 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1158,7 +1158,7 @@ function initWebSocket() {
     // Construye la URL completa y correcta para el WebSocket (ej: ws://192.168.5.52:8080)
     //const wsUrl = `ws://${wsHost}:8080`;
 
-    const cloudflareTunnelUrl = 'avatar-aquatic-blonde-older.trycloudflare.com'; // SOLO el hostname del túnel
+    const cloudflareTunnelUrl = 'handbook-restaurant-airplane-attribute.trycloudflare.com'; // SOLO el hostname del túnel
     const wsUrl = `wss://${cloudflareTunnelUrl}`;
 
     // Loguea la URL que se usará para la conexión (útil para depurar)
